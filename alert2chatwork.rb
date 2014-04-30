fba957f47cfd7e341813b9e60a3c9e0023679b4e
Kazuto Kusama <jacopen@gmail.com>
2014-04-30 15:03:19 +0900

    Refactoring
    1. Rubyは括弧を省略可能だが、pやprint以外のメソッドでは括弧を付ける方が
    好まれるので、修正
    
    2. current_directoryの時点で絶対パスになっているので、それ以降のexpand_pathを削除
    
    3. 複数回使っていないところは変数を使わずそのまま突っ込むようにした

--- alert2chatwork.rb
+++ alert2chatwork.rb
@@ -1,18 +1,16 @@
 #!/usr/local/bin/ruby
 require 'yaml'
-
-ENV['BUNDLE_GEMFILE'] = File.expand_path File.join(File.dirname($0), 'Gemfile')
 require 'bundler'
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path(File.join(File.dirname($0), 'Gemfile'))
 Bundler.require
 
-current_directory = File.expand_path File.dirname(__FILE__)
-require File.expand_path File.join(current_directory, 'lib/watcher.rb')
-require File.expand_path File.join(current_directory, 'lib/alerter.rb')
+current_directory = File.expand_path(File.dirname(__FILE__))
+require File.join(current_directory, 'lib/watcher.rb')
+require File.join(current_directory, 'lib/alerter.rb')
 
-list = File.expand_path File.join(current_directory, 'list.yml')
-watcher = Watcher.new list
+watcher = Watcher.new(File.join(current_directory, 'list.yml'))
 messages = watcher.watch
 
-config = File.expand_path File.join(current_directory, 'config.yml')
-alerter = Alerter.new config
+alerter = Alerter.new(File.join(current_directory, 'config.yml'))
 alerter.alert messages
